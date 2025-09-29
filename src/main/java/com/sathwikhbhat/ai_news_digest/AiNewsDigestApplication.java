58965942d69a0630d798dba3cf7654c585aa428b
Sathwik Hejamady Bhat <sathwikhbhat@gmail.com>
2025-09-29 16:09:12 +0530

    chore: initialize AI News Digest application with Spring Boot and Maven setup

---
+++ src/main/java/com/sathwikhbhat/ai_news_digest/AiNewsDigestApplication.java
@@ -0,0 +1,13 @@
+package com.sathwikhbhat.ai_news_digest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiNewsDigestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiNewsDigestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sathwikhbhat/ai_news_digest/AiNewsDigestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sathwikhbhat.ai_news_digest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiNewsDigestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
